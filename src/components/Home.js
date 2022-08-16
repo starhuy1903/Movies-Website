8022f37d607349a0932aeb6654b4749b66c39531
Huy Nguyen <huykst137@gmail.com>
2022-08-16 14:54:59 +0700

    Add Button component - styles - logics

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,33 +15,30 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
-import searchBar from "./SearchBar";
+import Button from "./Button";
 
 
 const Home = () => {
-    const {state, loading, error, searchTerm, setSearchTerm} = useHomeFetch();
+    const {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore} = useHomeFetch();
 
-    return (
-        <>
-            {!searchTerm && state.results[0] ? (
-                <HeroImage
+    if(error) return <div>Something went wrong...</div>
+
+    return (<>
+            {!searchTerm && state.results[0] ? (<HeroImage
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title={state.results[0].original_title}
-                    text={state.results[0].overview}/>
-            ) : null}
+                    text={state.results[0].overview}/>) : null}
 
             <SearchBar setSearchTerm={setSearchTerm}/>
 
             <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
-                {state.results.map(movie => (
-                    <Thumb key={movie.id} clickable
-                           image={movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-                               : NoImage} movieId={movie.id}></Thumb>
-                ))}
+                {state.results.map(movie => (<Thumb key={movie.id} clickable
+                                                    image={movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage}
+                                                    movieId={movie.id}></Thumb>))}
             </Grid>
-            <Spinner/>
-        </>
-    );
+            {loading && <Spinner/>}
+            {state.page < state.total_pages && !loading && (<Button callback={() => setIsLoadingMore(true)} text="Load More"></Button>)}
+        </>);
 };
 
 export default Home;
