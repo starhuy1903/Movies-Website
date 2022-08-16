217dcfd2191abe2d63c181cd9963b5ac6a4ec7bf
Huy Nguyen <huykst137@gmail.com>
2022-08-16 11:33:16 +0700

    Add Grid component and styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ import {useHomeFetch} from '../hooks/useHomeFetch'
 //Image
 import NoImage from '../images/no_image.jpg'
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 
 
 const Home = () => {
@@ -24,6 +25,12 @@ const Home = () => {
                     title={state.results[0].original_title}
                     text={state.results[0].overview}/>
             ) : null}
+
+            <Grid header="Popular Movies">
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
         </>
     );
 };
