51b17292061556fa9b8b53b5a8825b4c041298ff
Huy Nguyen <huykst137@gmail.com>
2022-08-19 20:29:58 +0700

    Add Context for auth

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ import Home from "./components/Home";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import MovieWithParams from "./components/Movie";
 import NotFound from "./components/NotFound";
+//Context
+import UserProvider from "./context";
 
 const App = () => {
     return (
         <Router>
-            <Header/>
-            <Routes>
-                <Route path="/" exact element={<Home/>}/>
-                <Route path="/:movieId" element={<MovieWithParams/>}/>
-                <Route path="/*" element={<NotFound/>}/>
-            </Routes>
+            <UserProvider>
+                <Header/>
+                <Routes>
+                    <Route path="/" exact element={<Home/>}/>
+                    <Route path="/:movieId" element={<MovieWithParams/>}/>
+                    <Route path="/*" element={<NotFound/>}/>
+                </Routes>
 
-            <GlobalStyle/>
+                <GlobalStyle/>
+            </UserProvider>
         </Router>
     );
 }
